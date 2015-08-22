b5f3efc6fcef0625b5db62c2f16c94adf42a528d
郑弋天 <ytzheng@corp.netease.com>
2015-08-22 14:22:00 +0800

    echarts库加入过滤规则

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ var config = {
 		tasks: [
 			// ftl动态假数据, RequireJS和jQuery(及其目录下的)也不做处理
 			{
-				test: /(?:\.ftl|require|jquery|jquery\/.*)\.js$/,
+				test: /(?:\.ftl|require|jquery|jquery\/.*|echarts|echarts\/.*)\.js$/,
 				plugins: []
 			},
 
@@ -78,7 +78,7 @@ var config = {
 					{
 						plugin: amdParse,
 						option: {
-							ignore: ['jquery']
+							ignore: ['jquery', /^echarts/]
 						}
 					},
 					inlineContentParse
